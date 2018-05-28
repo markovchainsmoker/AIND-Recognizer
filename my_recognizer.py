c230b958059a80abd1eccb4a807d8f47a94e37a5
markovchainsmoker <mr.kurt.osis@gmail.com>
2018-05-28 16:36:28 +0900

    shipping now

--- my_recognizer.py
+++ my_recognizer.py
@@ -27,14 +27,14 @@ def recognize(models: dict, test_set: SinglesData):
         top_prob, top_word = float("-inf"), None
         
         word_probabilities = {}
-        
+        #these 'probabilities' are actually the logL from .score()
         seq, lengths = test_set.get_item_Xlengths(index)
         for word, model in models.items():
             try:
                 word_probabilities[word] = model.score(seq, lengths)
             except Exception as e:
                 word_probabilities[word] = float("-inf")
-            
+            #select the most likely word
             if word_probabilities[word] > top_prob:
                 top_prob, top_word = word_probabilities[word], word
                 
